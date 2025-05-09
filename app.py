1867279697fd3de3784b705c55016805630159cc
Djin <32814285+jdnejal@users.noreply.github.com>
2025-05-09 17:55:50 +0200

    Scatter Plot for all data

--- app.py
+++ app.py
@@ -2,24 +2,14 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = pd.read_csv("./data/student-habits/student_habits_performance.csv")
 
 app = Dash()
 
-# Requires Dash 2.17.0 or later
 app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    html.H1(children='EduClimb', style={'textAlign':'center', 'font-family':'arial', 'color':'blue'}),
+    dcc.Graph(figure=px.scatter(df, x='study_hours_per_day', y='exam_score', size="attendance_percentage",size_max=10, color="gender", hover_data="student_id"))
 ]
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
     app.run(debug=True)
